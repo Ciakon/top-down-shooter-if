5135624f8e969197bae4b41c2d39e0b7cdbf7d37
magn9775 <magnusd2005@gmail.com>
2023-03-09 09:23:50 +0100

    playerIdleAdd

--- shooter-game/sketch.js
+++ shooter-game/sketch.js
@@ -1,16 +1,39 @@
+
+
+
+
+
+
+
+let player;
+let x=0;
+let y=0;
+function preload() {
+  player = loadImage('assets/playerShotgun.png');
+}
+
+
+
+
+
+
 function setup() {
   createCanvas(400, 400);
+  
 }
 
 function draw() {
   background(220);
+  x++
+  image(player, x, y, 50, 50);
+  
 }
 
 
 
 
 
-cheez
+
 
 
 
