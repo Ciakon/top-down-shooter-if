1aefcb8d09590141ef1e8d51d3e3f9ea31cba1fa
magn9775 <magnusd2005@gmail.com>
2023-03-09 09:26:38 +0100

    Update sketch.js

--- shooter-game/sketch.js
+++ shooter-game/sketch.js
@@ -9,7 +9,7 @@ let player;
 let x=0;
 let y=0;
 function preload() {
-  player = loadImage('assets/playerShotgun.png');
+  player = loadImage('assets/playerShotgunMove.gif');
 }
 
 
@@ -26,7 +26,7 @@ function draw() {
   background(220);
   x++
   image(player, x, y, 50, 50);
-  
+  //saveGif(playerShotgunMove, 100)
 }
 
 
